18f3022c22c5768034b6c9b97b69ededdaf20a95
Raccoon <vsafirko@gmail.com>
2023-10-26 20:12:48 +0300

    Changes in README

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Java-Pro-Course
-Repository with course homework
+## Repository with course homework a description of the assignment
+
+### Homework №3 Introduction to OOP
+This part of the course focuses on object-oriented programming concepts in Java. The code is organized into several packages and classes to demonstrate the following:
+
+- **Car and Employee Classes:** The `Car` class represents a simple car object, and the `Employee` class represents an employee with a car. The `Main` class in the `org.course.hw3` package demonstrates how these classes can be used together.
+
+- **Car Class:** The `Car` class has methods to start and stop the car, as well as display car information. It showcases encapsulation and object-oriented principles.
+
+- **Employee Class:** The `Employee` class represents an employee with properties like name, email, phone, position, and age. It also has an association with the `Car` class.
+
+- **Main Class:** The `Main` class demonstrates the usage of the `Car` and `Employee` classes by creating an employee with a car and starting/stopping the car.
+
+
+
+### Homework №4 Inheritance in Java
+This part of the course explores the use of abstract classes and interfaces to create a simple animal classification system in Java. The code is organized into different packages and classes, each showcasing a different approach:
+
+- **AbstractClass Package:** Demonstrates the use of abstract classes for animal classification. The `Main` class creates instances of dogs and cats, calculates the distance they can run and swim, and counts the total number of dogs and cats.
+
+- **Interface Package:** Shows the use of interfaces for animal classification. The `Main` class creates instances of dogs and cats, calculates the distance they can run and swim, and counts the total number of dogs and cats.
+
+- **Dog and Cat Classes:** Both the abstract and interface-based approaches have separate `Dog` and `Cat` classes, each implementing the respective behavior for running and swimming based on their type.
+
+- **Animals Abstract Class and Interface:** Defines the common structure and behavior for animals using an abstract class (`Animals`) and an interface (`Animals`). This includes properties like name, breed, and age, as well as methods for running and swimming.
+
+The `Main` class in each package demonstrates how these classes and interfaces are used to create and manage animal instances.
+
+### Homework №5 Polymorphism in Java
+In this part of the course, we explore polymorphism in Java. The program simulates a competition between participants and obstacles.
+
+- **Figures Package:** Demonstrates the use of interfaces to create different shapes, such as circles, squares, and triangles. These shapes calculate their respective areas and are stored in an array. The `Main` class calculates the total area of all shapes.
+
+- **Obstacle Package:** Introduces the concept of obstacles, such as running tracks and walls. These obstacles can be overcome by participants.
+
+- **Participant Package:** Defines different types of participants: `Person`, `Cat`, and `Robot`. Each participant has maximum running and jumping abilities.
+
+- **Main Class:** Combines participants and obstacles to simulate a competition. It calculates the success of each participant in overcoming obstacles and provides feedback based on their performance.
+
+### Homework №6 Primitive and reference data types
+- **printThreeWords():** A function that prints three words on the console: "Orange," "Banana," and "Apple."
+
+- **checkSumSign():** A function that checks the sign of the sum of two numbers and prints an appropriate message.
+
+- **printColor():** A function that prints a color status based on a given numeric value.
+
+- **compareNumbers():** A function that compares two numbers and prints the comparison result.
+
+- **isSumInRange(int num1, int num2):** A function that checks if the sum of two numbers is in the range from 10 to 20 (inclusive).
+
+- **checkNumber(int num):** A function that checks if a number is positive or negative and prints the appropriate message.
+
+- **isNegativeNum(int num):** A function that checks if a number is negative.
+
+- **printStringMultipleTimes(String text, int count):** A function that prints a given string a specified number of times.
+
+- **isLeapYear(int year):** A function that checks if a year is a leap year based on specified conditions.
--- src/main/java/org/course/hw6/Main.java
+++ src/main/java/org/course/hw6/Main.java
@@ -1,46 +1,168 @@
-package org.course.hw5;
+package org.course.hw6;
 
-import org.course.hw5.Obstacle.Obstacle;
-import org.course.hw5.Obstacle.RunningTrack;
-import org.course.hw5.Obstacle.Wall;
-import org.course.hw5.Participant.Cat;
-import org.course.hw5.Participant.Participant;
-import org.course.hw5.Participant.Person;
-import org.course.hw5.Participant.Robot;
 
 public class Main {
     public static void main(String[] args) {
-        Participant[] participants = new Participant[] {
-                new Person("Alice"),
-                new Cat("Whiskers"),
-                new Robot("Robo"),
-                new Robot("T-1000")
-        };
+        printThreeWords();
 
-        Obstacle[] obstacles = new Obstacle[] {
-                new RunningTrack(50),
-                new Wall(1),
-                new RunningTrack(200),
-                new Wall(4)
-        };
+        checkSumSign();
 
-        for (Participant participant : participants) {
-            boolean isParticipantStillInGame = true;
-            int countOfObstacle = 0;
+        printColor();
 
-            for (Obstacle obstacle : obstacles) {
-                if (!obstacle.overCome(participant)) {
-                    isParticipantStillInGame = false;
-                    break;
-                }
+        compareNumbers();
 
-                countOfObstacle++;
-            }
-            if (isParticipantStillInGame) {
-                System.out.println("Participant[" + participant.getName() + "] completes all obstacles!");
-            } else {
-                System.out.println("Participant[" + participant.getName() + "] completes " + countOfObstacle + " obstacles.");
-            }
+        int a = 5;
+        int b = 15;
+        System.out.println(isSumInRange(a, b));
+
+        checkNumber(a);
+        checkNumber(a - 20);
+
+        System.out.println(isNegativeNum(b));
+
+        printStringMultipleTimes("Hello world", 5);
+        printStringMultipleTimes("Hello world", 0);
+
+
+        System.out.println(isLeapYear(2000));
+        System.out.println(isLeapYear(2004));
+        System.out.println(isLeapYear(2005));
+        System.out.println(isLeapYear(2008));
+    }
+
+    // Task 1
+    /**
+     * Prints three words on the console: "Orange", "Banana", and "Apple"
+     */
+    public static void printThreeWords() {
+        System.out.println("Orange\n" +
+                "\n" +
+                "Banana\n" +
+                "\n" +
+                "Apple");
+    }
+
+    // Task 2
+    /**
+     * Checks the sign of the sum of two numbers and prints an appropriate message
+     */
+    public static void checkSumSign() {
+        int a = 100;
+        int b = -99;
+
+        if (a + b > 0) {
+            System.out.println("Сума позитивна");
+            return;
+        } else {
+            System.out.println("Сума негативна");
         }
+    }
+
+    // Task 3
+    /**
+     * Prints a color status based on the "value" parameter
+     */
+    public static void printColor() {
+        int value = 90;
+
+        if (value <= 0) {
+            System.out.println("Червоний");
+        }
+
+        if (value > 0 && value <= 100) {
+            System.out.println("Жовтий");
+        }
+
+        if (value > 100) {
+            System.out.println("Зелений");
+        }
+    }
+
+    // Task 4
+    /**
+     * Compares two numbers and prints the comparison result.
+     */
+    public static void compareNumbers() {
+        int a = 100;
+        int b = -99;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    // Task 5
+    /**
+     * Checks if the sum of two numbers is in the range from 10 to 20 (inclusive).
+     *
+     * @param num1 The first number for addition.
+     * @param num2 The second number for addition.
+     * @return `true` if the sum is in the range, `false` otherwise.
+     */
+    public static boolean isSumInRange(int num1, int num2) {
+        return (num1 + num2) > 10 && (num1 + num2) <= 20;
+    }
+
+    // Task 6
+    /**
+     * Checks if a number is positive or negative and prints the appropriate message.
+     *
+     * @param num The number to check.
+     */
+    public static void checkNumber(int num) {
+        if (num >= 0) {
+            System.out.println("Ви передали додатне число");
+            return;
+        }
+
+        System.out.println("Ви передали від'ємне число");
+    }
+
+    // Task 7
+    /**
+     * Checks if a number is negative.
+     *
+     * @param num The number to check.
+     * @return `true` if the number is negative, `false` otherwise.
+     */
+    public static boolean isNegativeNum(int num) {
+        if (num < 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Task 7
+    /**
+     * Prints a given string a specified number of times.
+     *
+     * @param text  The string to print.
+     * @param count The number of times to print the string.
+     */
+    public static void printStringMultipleTimes(String text, int count) {
+        if (count <= 0) {
+            System.out.println("\nНе можу надрукувати меньше, а ніж один раэ");
+            System.out.println(text);
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println(text);
+        }
+    }
+
+    // Task 8
+    /**
+     * Checks if a year is a leap year based on the specified conditions.
+     *
+     * @param year The year to check for leap year status.
+     * @return `true` if the year is a leap year, `false` otherwise.
+     */
+    public static boolean isLeapYear(int year) {
+        if ((year % 100 != 0 && year % 4 == 0) || year % 400 == 0)  {
+            return true;
+        }
+        return false;
     }
 }
