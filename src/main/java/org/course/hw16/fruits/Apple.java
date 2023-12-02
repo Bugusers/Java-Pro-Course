545837c4e89ed0e6e7f8e490ae4964bfa656ec0d
Raccoon <vsafirko@gmail.com>
2023-12-02 18:30:09 +0200

    Done hw16

---
+++ src/main/java/org/course/hw16/fruits/Apple.java
@@ -0,0 +1,9 @@
+package org.course.hw16.fruits;
+
+public class Apple extends Fruit {
+    public static final float ONE_APPLE_WEIGHT = 1.0F;
+
+    public Apple() {
+        super(ONE_APPLE_WEIGHT);
+    }
+}
